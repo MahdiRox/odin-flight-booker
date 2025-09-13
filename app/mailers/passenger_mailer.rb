ebd8135dbd92dda6c8fd8f7453bd6743277fa42e
mahdi <90812003+MahdiRox@users.noreply.github.com>
2025-09-13 15:58:34 +0100

    another email fix

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,7 +1,7 @@
 class PassengerMailer < ApplicationMailer
   default from: 'zohrafarid3123@gmail.com'
   layout 'mailer'
-  def confirmation_email(passenger)
+  def confirmation_email
     @passenger = params[:passenger] 
     mail(to: @passenger.email, subject: "booking notification")
   end
